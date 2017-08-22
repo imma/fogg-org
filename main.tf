7a3b5e15dbd6a5a4980250afe6e70a762fc883e4
Defn Nghiem <iam@defn.sh>
2017-08-22 10:31:11 +0000

    api gateway  needs a role to write to cloudwatch logs

--- main.tf
+++ main.tf
@@ -660,3 +660,53 @@ resource "aws_iam_role_policy_attachment" "macie_setup_cloudtrail" {
   policy_arn = "arn:aws:iam::aws:policy/AWSCloudTrailReadOnlyAccess"
   count      = "${var.want_macie}"
 }
+
+resource "aws_api_gateway_account" "org" {
+  cloudwatch_role_arn = "${aws_iam_role.api_gateway.arn}"
+}
+
+resource "aws_iam_role" "api_gateway" {
+  name = "api_gateway_cloudwatch"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "apigateway.amazonaws.com"
+        },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "api_gateway" {
+  name = "default"
+  role = "${aws_iam_role.api_gateway.id}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:DescribeLogGroups",
+        "logs:DescribeLogStreams",
+        "logs:PutLogEvents",
+        "logs:GetLogEvents",
+        "logs:FilterLogEvents"
+        ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
